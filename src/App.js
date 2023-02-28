2b33e0cd915c181961b2b79bb52d9825a23724a7
2020BTEIT00040 <86112650+2020BTEIT00040@users.noreply.github.com>
2023-02-28 19:47:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+// Components
+import Login from './Components/Account/Login'
+
+function App() {
+  return (
+    <div style={{ marginTop: 64 }}>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
